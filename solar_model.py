835bb812cdd7a92962fa07626a043f0c71c5e521
Dobrovenskis <dobrovenskis.rv@phystech.edu>
2020-11-10 16:19:27 +0300

    Fix

--- solar_model.py
+++ solar_model.py
@@ -19,10 +19,8 @@ def calculate_force(body, space_objects):
         if body == obj:
             continue  # тело не действует гравитационной силой на само себя!
         r = ((body.x - obj.x) ** 2 + (body.y - obj.y) ** 2) ** 0.5
-        body.Fx += gravitational_constant * ((obj.m * body.m) / (r * r)) * (
-                (obj.x - body.x) / r)  # FIXME: нужно вывести формулу...
-        body.Fy += gravitational_constant * ((obj.m * body.m) / (r * r)) * (
-                (obj.y - body.y) / r)  # FIXME: нужно вывести формулу...
+        body.Fx += gravitational_constant * ((obj.m * body.m) / (r * r)) * ((obj.x - body.x) / r)  # FIXME: нужно вывести формулу...
+        body.Fy += gravitational_constant * ((obj.m * body.m) / (r * r)) * ((obj.y - body.y) / r)  # FIXME: нужно вывести формулу...
 
 
 def move_space_object(body, dt):
@@ -35,11 +33,10 @@ def move_space_object(body, dt):
 
     ax = body.Fx / body.m
     ay = body.Fy / body.m
-    body.Vx += ax * dt
-    body.Vx += ay * dt
     body.x += body.Vx * dt + ax * (dt ** 2) / 2
     body.y += body.Vy * dt + ay * (dt ** 2) / 2
-
+    body.Vx += ax * dt
+    body.Vy += ay * dt
 
 def recalculate_space_objects_positions(space_objects, dt):
     """Пересчитывает координаты объектов.
